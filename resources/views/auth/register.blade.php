f6dce2281b82b8046e1b229da92063e1a9cbc503
Angelica Tanagras <angelicatanagras@gmail.com>
2020-04-21 01:42:39 +0800

    Updated Login and Register UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header" style="background-image: url('/app-assets/img/grocery-02.jpg'); padding: 9rem;">
+                    {{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -63,7 +64,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn bg-olive">
                                     {{ __('Register') }}
                                 </button>
                             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="{{asset('app-assets/plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bbootstrap 4 -->
+    <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet"
           href="{{asset('app-assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="{{asset('app-assets/plugins/jqvmap/jqvmap.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('app-assets/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('app-assets/dist/css/styles.css')}}">
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="{{asset('app-assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
@@ -27,6 +28,9 @@
     <link rel="stylesheet" href="{{asset('app-assets/plugins/summernote/summernote-bs4.css')}}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+
+
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 
